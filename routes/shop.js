3f1505e69b20b7ee6e3b2bc7319d96fa381a9c1c
wanko <kouji124@w4.dion.ne.jp>
2021-05-04 12:00:16 -0700

    deleted comment

--- routes/shop.js
+++ routes/shop.js
@@ -7,20 +7,6 @@ router.get('/', (req, res, next) => {
   res.send('<h1>Hello ffrom the other side</h1>')
 })
 
-// router.use('/add-products', (req, res) => {
-//   res.send(`
-//   <form action="/product" method="POST">
-//     <input type="text" name="title" /> 
-//     <button type="submit">SEND</button>
-//   </form>
-//   `)
-// })
-
-// router.post('/product', (req, res) => {
-//   console.log(req.body);
-//   res.redirect('/')
-// })
-
 router.get("*", (req, res) => {
   console.log("yoyoyoyo");
   res.sendFile(path.join(__dirname,'../', 'public', '404.html'))
